abfe9f6aaec166060d7c7f788808a9d7fe1db6b9
Banyc <36535895+Banyc@users.noreply.github.com>
2022-09-30 18:38:53 +0800

    feat: test cases cover Val

--- src/value.rs
+++ src/value.rs
@@ -29,7 +29,7 @@ impl<'buf> Val<'buf> {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Error {
     InvalidType,
 }
@@ -38,4 +38,25 @@ pub enum Error {
 pub struct ValInfo<'buf> {
     pub value: Val<'buf>,
     pub pos: usize,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let val = Val::VarInt(0x1234);
+        assert_eq!(val.varint().unwrap(), 0x1234);
+
+        let val = Val::Bytes(Cow::from(vec![1, 2, 3]));
+        assert_eq!(val.varint().unwrap_err(), Error::InvalidType);
+
+        let val = Val::VarInt(0x1234);
+        assert_eq!(val.bytes().unwrap_err(), Error::InvalidType);
+        assert_eq!(val.into_bytes().unwrap_err(), Error::InvalidType);
+
+        let val = Val::Bytes(Cow::from(vec![1, 2, 3]));
+        assert_eq!(val.into_bytes().unwrap(), vec![1, 2, 3]);
+    }
 }
